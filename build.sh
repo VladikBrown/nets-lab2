8795d2ee7fb55b616b88d2951aea9f51f9b6748e
VladikBrown <vladikbrown@gmail.com>
2021-05-11 22:10:00 +0300

    made docker usage more convenient

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 (cd deployment/postgresql && docker-compose up -d) &&
 mvn clean package &&
 (cd deployment/postgresql && docker-compose down) &&
-rm src/main/docker/lab2-0.0.1-SNAPSHOT.jar
-cp target/lab2-0.0.1-SNAPSHOT.jar src/main/docker/lab2-0.0.1-SNAPSHOT.jar
+rm lab2-0.0.1-SNAPSHOT.jar
+cp target/lab2-0.0.1-SNAPSHOT.jar lab2-0.0.1-SNAPSHOT.jar
